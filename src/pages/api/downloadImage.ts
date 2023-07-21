149d3ee46705e5d7bf720719a78f2741daa6dfde
Ericsen Tsai <bommer95175@gmail.com>
2023-07-22 01:44:05 +0800

    refactor: modify logic to get instagram image

---
+++ src/pages/api/downloadImage.ts
@@ -0,0 +1,30 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import axios from "axios";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const url = req.query.url as string;
+
+  if (typeof url !== "string") {
+    res.status(400).json({ error: "Invalid URL" });
+  }
+
+  await axios({
+    method: "GET",
+    url,
+    responseType: "arraybuffer",
+  })
+    .then((response) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const buffer = Buffer.from(response.data, "binary");
+      res.setHeader("Content-Type", "image/jpeg");
+      res.setHeader("Content-Disposition", 'attachment; filename="image.jpg"');
+      res.send(buffer);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: "Failed to download image" });
+    });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-import { api } from "@/utils/api";
-
 const urlRegex = new RegExp(
   "^((https?|ftp|file)://)?" + // protocol
     "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}|" + // domain name
@@ -16,29 +14,21 @@ const urlRegex = new RegExp(
 );
 
 const Home: NextPage = () => {
+  const [input, setInput] = useState<string>("");
   const [url, setUrl] = useState<string>("");
-  const instagramResult = api.instagram.imageFetcher.useQuery(
-    { url },
-    {
-      enabled: false,
-      refetchOnWindowFocus: false,
-    }
-  );
 
   const isUrlValid = !url || urlRegex.test(url);
 
   const router = useRouter();
 
-  const [imageBlobUrl, setImageBlobUrl] = useState<string | null>(null);
-
-  const handleDownloadClick = async () => {
-    if (!instagramResult.data?.imgSrc) return;
-    const response = await fetch(instagramResult.data?.imgSrc);
-    const blob = await response.blob();
-    const blobUrl = URL.createObjectURL(blob);
-    setImageBlobUrl(blobUrl);
+  const handleImageClick = () => {
+    const link = document.createElement("a");
+    link.href = `/api/downloadImage?url=${encodeURIComponent(url)}`;
+    link.download = "image.jpg";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
-
   return (
     <>
       <Head>
@@ -50,53 +40,53 @@ const Home: NextPage = () => {
         <h1 className="mb-5 text-center text-[4.2rem] font-bold text-neutral md:text-[6rem]">
           IG Image Downloader
         </h1>
-        {(!instagramResult.isFetched || instagramResult.isError) && (
-          <div className="flex w-1/2 min-w-[25rem] flex-wrap items-center justify-center gap-3 text-[3.2rem]">
-            <div className="relative flex items-center justify-center">
-              <input
-                type="text"
-                placeholder="Type here"
-                className={`input-bordered input input-lg w-full max-w-md ${
-                  isUrlValid ? "input-neutral" : "input-error"
-                }`}
-                value={url}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setUrl(e.target.value)
-                }
-                disabled={instagramResult.isFetching}
-              />
-              {instagramResult.isFetching && (
-                <progress className="progress absolute bottom-[-1px] left-1/2 h-[3px] w-[85%] -translate-x-1/2"></progress>
-              )}
-            </div>
-            <button
-              className="btn-accent btn-lg btn text-neutral"
-              disabled={!isUrlValid || instagramResult.isFetching}
-              onClick={() => void instagramResult.refetch()}
-            >
-              GET the image!!
-            </button>
-          </div>
-        )}
-        {instagramResult.isFetched && !!instagramResult.data?.imgSrc && (
+        <div className="flex w-1/2 min-w-[25rem] flex-wrap items-center justify-center gap-3 text-[3.2rem]">
+          {!url && (
+            <>
+              <div className="relative flex items-center justify-center">
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className={`input-bordered input input-lg w-full max-w-md ${
+                    isUrlValid ? "input-neutral" : "input-error"
+                  }`}
+                  value={input}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setInput(e.target.value)
+                  }
+                />
+              </div>
+              <button
+                className="btn-accent btn-lg btn text-neutral"
+                disabled={!isUrlValid}
+                onClick={() => {
+                  const url = new URL(input);
+                  console.log(url.origin + url.pathname + "media?size=l");
+                  void setUrl(url.origin + url.pathname + "media?size=l");
+                }}
+              >
+                GET the image!!
+              </button>
+            </>
+          )}
+        </div>
+        {url && (
           <>
             <div className="relative mt-10 h-[30rem] w-full">
               <Image
-                src={instagramResult.data?.imgSrc}
+                src={url}
                 alt="the image you want"
                 fill
                 style={{ objectFit: "contain" }}
               ></Image>
             </div>
             <div className="mt-3 flex items-center gap-3">
-              <a
+              <button
                 className="btn-accent btn-lg btn"
-                href={instagramResult.data.imgSrc}
-                download="image.png"
-                target="_blank"
+                onClick={handleImageClick}
               >
-                Open image in new window
-              </a>
+                DOWNLOAD!
+              </button>
               <button
                 className="btn-secondary btn-lg btn"
                 onClick={() => void router.reload()}
